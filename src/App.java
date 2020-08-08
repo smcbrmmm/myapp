56cf28bd89f74f64669ba00d52de8cd190fe9c8b
Samut Chouybumrung <samut.c@ku.th>
2020-08-08 13:19:26 +0700

    bring back src/App.java from commit b7f9427

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
-import java.sql.SQLOutput;
-
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello again");
     }
 }
