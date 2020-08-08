56dbdd92853fc17bcca78b5270f7872bcf08cbca
Samut Chouybumrung <samut.c@ku.th>
2020-08-08 13:12:37 +0700

    Update App and add Animal

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello again");
     }
 }
